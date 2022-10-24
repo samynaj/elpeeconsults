773abbc530261232d8481de4c3a035a661d60293
samuel nnaji <samuelnnj@gmail.com>
2022-10-25 00:46:35 +0100

    feat: added navbar

---
+++ pages/index.jsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Emwesto</title>
+        <meta name="description" content="Global Service Limited" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.hero}>
+          <div className={styles.navbar}>
+            <div className={styles.logo}>
+              Emwesto
+            </div>
+            <div className={styles.nav}>
+              <span>Home</span>
+              <span>About</span>
+              <span>Services</span>
+              <span>Projects</span>
+              <span>Contacts</span>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
