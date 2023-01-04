47555d5c86a7aac945d0e2450332d0ceb290cb40
samuel nnaji <samuelnnj@gmail.com>
2023-01-04 02:51:47 +0100

    final fix for now

--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -1,46 +1,72 @@
 import Image from 'next/image'
 import React from 'react'
 import styles from './index.module.scss'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, EffectCube, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
 
 const Index = () => {
     return (
         <div id='projects' className={styles.root}>
-            <div className={styles.top}>
-                <h2>Our Projects</h2>
-                <div></div>
-                <p>Get an overview of some of our current projects</p>
-            </div>
-            <div className={styles.projects}>
-                <div className={styles.topP}>
-                    <div data-aos="flip-left"
+            <div className={styles.bg}>
+                <div className={styles.content}>
+                    <div className={styles.top}>
+                        <h2 data-aos='flip-left'
+                            data-aos-easing="ease-out-cubic"
+                            data-aos-duration="3000">Our Projects</h2>
+                        <div></div>
+                        <p>Get an overview of some of our current projects</p>
+                    </div>
+                    <div data-aos="flip-right"
                         data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" className={styles.size2}> <img src="/assets/pro1.png" alt="" /></div>
-                    <div
-                        data-aos="flip-right"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" className={styles.size1}> <img src="/assets/pro2.png" alt="" /></div>
-                </div>
-                <div className={styles.center}>
-                    <div data-aos="flip-left"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" className={styles.size1}> <img src="/assets/pro3.png" alt="" /></div>
-                    <div data-aos="flip-up"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" className={styles.size1}> <img src="/assets/pro4.png" alt="" /></div>
-                    <div
-                        data-aos="flip-right"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000"
-                        className={styles.size1}> <img src="/assets/pro1.png" alt="" /></div>
-                </div>
-                <div className={styles.bottom}>
-                    <div data-aos="flip-left"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" className={styles.size1}> <img src="/assets/pro2.png" alt="" /></div>
-                    <div
-                        data-aos="flip-right"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" className={styles.size2}> <img src="/assets/pro3.png" alt="" /></div>
+                        data-aos-duration="2000" className={styles.right}>
+                        <Swiper
+                            style={{ zIndex: 0 }}
+                            // breakpoints={{
+                            //     // when window width is >= 640px
+                            //     640: {
+                            //         width: 640,
+                            //         slidesPerView: 1,
+                            //     },
+                            //     // when window width is >= 768px
+                            //     768: {
+                            //         width: 768,
+                            //         slidesPerView: 2,
+                            //     },
+                            //     1260: {
+                            //         width: 1260,
+                            //         slidesPerView: 3,
+                            //     },
+                            // }}
+                            // spaceBetween={0}
+                            effect={"cube"}
+                            grabCursor={true}
+                            cubeEffect={{
+                                shadow: true,
+                                slideShadows: true,
+                                shadowOffset: 20,
+                                shadowScale: 0.94,
+                            }}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            pagination={true}
+                            modules={[Navigation, EffectCube, Autoplay]}
+                            className={styles.swiper_container}
+                        >
+                            <SwiperSlide><img src="/assets/pro5 (1).jpeg" alt="" /></SwiperSlide>
+                            <SwiperSlide> <img src="/assets/pro5 (4).jpeg" alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="/assets/pro5 (13).jpeg" alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="/assets/pro5 (10).jpeg" alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="/assets/pro5 (14).jpeg" alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="/assets/pro5 (2).jpeg" alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="/assets/pro5 (11).jpeg" alt="" /></SwiperSlide>
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </div>
